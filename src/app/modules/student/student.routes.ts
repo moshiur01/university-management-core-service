3ea3221bc8828a9b2ce3f12450e34d713a2d52a7
moshiur01 <mr.moshiur314@gmail.com>
2023-09-01 12:18:01 +0600

    [DONE] Student update and delete added

---
+++ src/app/modules/student/student.routes.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { StudentController } from './student.controller';
+import { StudentValidation } from './student.validations';
+
+const router = express.Router();
+
+router.get('/', StudentController.getAllFromDB);
+
+router.get('/:id', StudentController.getByIdFromDB);
+
+router.post(
+  '/',
+  validateRequest(StudentValidation.create),
+  StudentController.insertIntoDB
+);
+
+router.patch(
+  '/:id',
+  validateRequest(StudentValidation.update),
+  StudentController.updateIntoDB
+);
+
+router.delete('/:id', StudentController.deleteFromDB);
+
+export const studentRoutes = router;
