8ef273295003fb4022da4c3aed6c57e390f2bf16
moshiur01 <mr.moshiur314@gmail.com>
2023-09-07 12:38:27 +0600

    [DONE] crate building zod validation and user role set

--- src/app/modules/building/building.route.ts
+++ src/app/modules/building/building.route.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { buildingController } from './building.controller';
+import { buildingValidation } from './building.validation';
 
 const router = express.Router();
 
-router.post('/', buildingController.insertIntoDB);
+router.post(
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  validateRequest(buildingValidation.create),
+  buildingController.insertIntoDB
+);
 router.get('/', buildingController.getAllFromDB);
 
 export const buildingRoutes = router;
