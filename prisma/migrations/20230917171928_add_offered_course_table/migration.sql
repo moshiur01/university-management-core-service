95059a066a44b762921d710baded5355562e6d5c
moshiur01 <mr.moshiur314@gmail.com>
2023-09-17 23:21:08 +0600

    [DONE] Offered Courese schema created

---
+++ prisma/migrations/20230917171928_add_offered_course_table/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "offered_courses" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "courseId" TEXT NOT NULL,
+    "academicDepertmentId" TEXT NOT NULL,
+    "semesterRegistrationId" TEXT NOT NULL,
+
+    CONSTRAINT "offered_courses_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "offered_courses" ADD CONSTRAINT "offered_courses_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "courses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "offered_courses" ADD CONSTRAINT "offered_courses_academicDepertmentId_fkey" FOREIGN KEY ("academicDepertmentId") REFERENCES "academic_departments"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "offered_courses" ADD CONSTRAINT "offered_courses_semesterRegistrationId_fkey" FOREIGN KEY ("semesterRegistrationId") REFERENCES "semester_registration"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
